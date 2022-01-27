c915b0f08ef77a2af32f61b5ca60e0c5d983a3be
bambiku <80384379+bambikuu@users.noreply.github.com>
2022-01-27 15:10:43 +0100

    Update __init__.py

--- src/dbdd/__init__.py
+++ src/dbdd/__init__.py
@@ -7,48 +7,7 @@ __version__ = '0.12'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 import logging
-from typing import NamedTuple, Literal
-
-from .client import *
-from .appinfo import *
-from .user import *
-from .emoji import *
-from .partial_emoji import *
-from .activity import *
-from .channel import *
-from .guild import *
-from .flags import *
-from .member import *
-from .message import *
-from .asset import *
-from .errors import *
-from .permissions import *
-from .role import *
-from .file import *
-from .colour import *
-from .integrations import *
-from .invite import *
-from .template import *
-from .widget import *
-from .object import *
-from .reaction import *
-from . import utils, opus, abc, ui
-from .enums import *
-from .embeds import *
-from .mentions import *
-from .shard import *
-from .player import *
-from .webhook import *
-from .voice_client import *
-from .audit_logs import *
-from .raw_models import *
-from .team import *
-from .sticker import *
-from .stage_instance import *
-from .interactions import *
-from .components import *
-from .threads import *
-
+from typing import *
 
 class VersionInfo(NamedTuple):
     major: int
